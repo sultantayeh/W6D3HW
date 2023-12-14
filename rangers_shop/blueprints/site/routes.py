7591eaf7b82f898ce90305e461abfd979bceb037
Sultan Tayeh <tayehsultan@gmail.com>
2023-12-14 18:20:19 -0500

    commit

--- rangers_shop/blueprints/site/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-site = Blueprint('site', __name__, template_folder='site_templates')
-
-@site.route('/')
-def shop():
-    return render_template('shop.html')
